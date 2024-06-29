46735704e2fb9c83531a2d0f9278c09244e5a231
Udara Sampath <udarasampathw196841@gmail.com>
2024-06-29 06:12:23 +0530

    server script updates with port changes and other updates

--- py-websockets/server.py
+++ py-websockets/server.py
@@ -4,11 +4,17 @@ import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Define the IP address and port to listen on
-# Listen on all available interfaces and port 12345
-server_address = ('', 12345)
+# Listen on all available interfaces and port 50300
+s_port = 50300
+server_address = ('', s_port)
 
 # Bind the socket to the server address
 server_socket.bind(server_address)
+
+# Get the actual IP address the socket is bound to
+host_ip = socket.gethostbyname(socket.gethostname())
+print(f"Server IP address: {host_ip}")
+print(f"Server Port: {s_port}")
 
 # Listen for incoming connections
 server_socket.listen(1)
